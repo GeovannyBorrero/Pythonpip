4a731a32def42c85e32153b7124c1acda9b053d4
Geovanny Borrero <geovannyborrero@gmail.com>
2024-04-07 23:23:57 -0500

    se agregaron los archivos README y gitignore

--- hola.py
+++ hola.py
@@ -1,13 +1,17 @@
-num1 = int(input("digita el numero 1"))
-num2 = int(input("digita el numero 2"))
-opertation = input("digite que operacion deseas realiza, +,-,*,/")
-
-def calculadora(num1,num2,opertation):
-    if opertation == "+":
-        return num1 + num2
-     if opertation == "-":
-        return num1 - num2
-    if opertation == "*":
-        return num1 * num2
+def calculadora(num1,num2,operatation):
+    if operatation == '+':
+        print(num1 + num2)
+    elif operatation == "-":
+        print (num1 - num2)
+    elif operatation == "*":
+        print (num1 * num2)
     else:
-        return num1 / num2   
+        print (num1 / num2 )  
+
+
+
+
+numero1 = int(input("digita el numero 1 => "))
+numero2 = int(input("digita el numero 2 => "))
+operacion = input("digite que operacion deseas realiza, +,-,*,/ => ")
+calculadora(numero1,numero2,operacion)
